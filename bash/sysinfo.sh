15be20855f3771276f5aea115eb4cd60cdbe4589
Prajwol Adhikari <projaladhikary007@gmail.com>
2023-01-30 19:16:18 -0500

    Lab Completed

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -4,6 +4,6 @@ echo "FQDN: $(hostname)"
 echo "Host Information: "
 hostnamectl status | sed -n 1,9p 	#I'm using sed with -n option to print line 1-9 filtering the large output of hostnamectl
 echo "IP Addresses: "
-hostname -I | awk '{print$1" " $2" " $3}'
+hostname -I | awk '{print$1" " $2" " $3}' # Here I'm bringing host IP address(Not Local) and printing just three ip in case of many ip address.
 echo "Root Filesystem Status: "
-df -h /
+df -h / 		#Here I'm using df with option -h to bring the disk usgage output in Human Readable Form of partition mounted on /
