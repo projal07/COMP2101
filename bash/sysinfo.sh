9f672a532f302b1eb102f16bb612d6bef7caf962
Prajwol Adhikari <projaladhikary007@gmail.com>
2023-02-15 19:20:24 -0500

    Lab 2 Completed

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#First We are creating variable.
 Hostname=$(hostname -f)
 Os_name_version=$(hostnamectl status| sed -n 's/^Operating System: //p')
 IP=$(ip route | head -n1 | awk '{print$3}')
 root_space=$(df -h / | tail -n 1 | awk '{print$3}')
+#Now we are using cat to write in the end of file and using the above variable for required output
 cat << EOF
 
 Report for $(hostname)
